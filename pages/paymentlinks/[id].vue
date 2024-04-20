5c213eca1792d96964fdfc0d734ccd168e82bac1
“charles2k16” <charlesprogram@gmail.com>
2024-04-20 15:32:56 +0000

    ree

--- pages/paymentlinks/[id].vue
+++ pages/paymentlinks/[id].vue
@@ -1,53 +1,46 @@
 <template>
   <div class="w-full flex flex-col section">
-    <!-- Banner -->
-    <!-- <CampaignBanner class="hidden sm:block" /> -->
-    <!-- Campaign Info & Payment -->
-    <div class="flex lg:flex-row flex-col justify-center  md:space-x-4 h-full lg:mt-10">
-
-
-      <div class="lg:w-[50%] w-full  h-fit rounded-md flex justify-center">
-            <div class="lg:max-w-md md:max-w-2xl w-full flex justify-center bg-slate-300 ">
-               <PaymentLinkMerchantInfo :paymentOptions="paymentOptions!" :countries="cty_abbr"  />
-
-            </div>
-
+    <div class="flex lg:flex-row flex-col justify-center md:space-x-4 h-full lg:mt-10">
+      <div class="lg:w-[50%] w-full h-fit rounded-md flex justify-center">
+        <div class="lg:max-w-md md:max-w-2xl w-full flex justify-center bg-slate-300">
+          <PaymentLinkMerchantInfo
+            :paymentOptions="paymentOptions!"
+            :countries="cty_abbr" />
         </div>
       </div>
 
-      <!-- Campaign Info -->
-      <div class="lg:w-[50%] w-full hidden sm:block md:flex md:justify-center pt-5 lg:pt-0">
-      <div class="lg:max-w-md md:max-w-2xl w-full">
-        <PaymentLinkPaymentForm :paymentOptions="paymentOptions!" :countries="cty_abbr"  />
-      </div>
+      <div
+        class="lg:w-[50%] w-full hidden sm:block md:flex md:justify-center pt-5 lg:pt-0">
+        <div class="lg:max-w-md md:max-w-2xl w-full">
+          <PaymentLinkPaymentForm
+            :paymentOptions="paymentOptions!"
+            :countries="cty_abbr" />
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 <script setup lang="ts">
-import { useCampaignStore } from "~/store/campaign";
-import { usePaymentOptions } from "~/store/payment_options";
-
-
+import { useCampaignStore } from '~/store/campaign';
+import { usePaymentOptions } from '~/store/payment_options';
 
 const campaignStore = useCampaignStore();
-const route = useRoute()
-const paymentOptiosnStore = usePaymentOptions()
-const { paymentOptions } = storeToRefs(paymentOptiosnStore)
+const route = useRoute();
+const paymentOptiosnStore = usePaymentOptions();
+const { paymentOptions } = storeToRefs(paymentOptiosnStore);
 import { supportedCountries } from '~/assets/data';
 
 // data
-let cty_abbr = ['GH']
+let cty_abbr = ['GH'];
 
 // onmounted
 onMounted(() => {
-  campaignStore.verifyCampaignLink(route.params.id.toString())
-   paymentOptiosnStore.getPaymentMethod('GHS')
-  console.log(route.params.id)
-  getCountriesAsync()
-  console.log(cty_abbr)
-})
+  campaignStore.verifyCampaignLink(route.params.id.toString());
+  paymentOptiosnStore.getPaymentMethod('GHS');
+  console.log(route.params.id);
+  getCountriesAsync();
+  console.log(cty_abbr);
+});
 
 // methods
 function getCountriesAsync() {
