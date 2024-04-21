14c7c0b34df003eeabc408eb537119b445876150
“charles2k16” <charlesprogram@gmail.com>
2024-04-21 12:30:50 +0000

    set up api management

---
+++ repository/modules/paymentMethods.ts
@@ -0,0 +1,26 @@
+// 3rd's
+// 3rd's
+import type { FetchOptions } from 'ofetch';
+import type { AsyncDataOptions } from '#app';
+import type { PaymentMethods } from '~/types/index';
+
+// locals
+import FetchFactory from '../factory';
+
+class PaymentMethodsModule extends FetchFactory<PaymentMethods[]> {
+  private RESOURCE = '/paymentlinks/paymentmethods';
+
+  /**
+   * Return the data as array 
+   * @param asyncDataOptions options for `useAsyncData`
+   * @returns 
+   */
+  async getPaymentMethods ( currency: String ) {
+    return await this.call(
+      'GET',
+      `${ this.RESOURCE }?currency=${ currency }`
+    )
+  }
+}
+
+export default PaymentMethodsModule;
