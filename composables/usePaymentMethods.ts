e6e51e7438d23f7f7528885e700a875d92a41db7
“charles-brij” <charles@brij.africa>
2024-04-25 13:57:03 +0000

    add pusher

--- composables/usePaymentMethods.ts
+++ composables/usePaymentMethods.ts
@@ -1,39 +1,39 @@
-import {ref} from 'vue';
+import { ref } from 'vue';
 import type { PaymentMethods, PaymentOption } from '~/types';
 
-export default function usePaymentMethods(){
+export default function usePaymentMethods () {
 
-    // instance of api
-    const {$api} = useNuxtApp()
+  // instance of api
+  const { $api } = useNuxtApp()
 
-    const isPaymentMethodDataLoading = ref(false)
-    const isGetPaymentMethodsSuccessfull =ref(false)
-    const paymentMethods = ref<PaymentOption[] | null>();
-    
-    async function getPaymentMethod ( currency: string ) {
-        try {
-          console.log(currency)
-        isPaymentMethodDataLoading.value = true;
-          const  res = await $api.paymentMethods.getPaymentMethods(currency)
-          console.log(res)
-          if ( res.status === 200 ) {
-            paymentMethods.value = res.data;
-            isGetPaymentMethodsSuccessfull.value = true;
-            isPaymentMethodDataLoading.value = false;
-          } else {
-            isPaymentMethodDataLoading.value = false;
-          }
-        } catch ( error: any ) {
-          console.log(error)
-          
-          isPaymentMethodDataLoading.value = false;
-        }
+  const isPaymentMethodDataLoading = ref( false )
+  const isGetPaymentMethodsSuccessfull = ref( false )
+  const paymentMethods = ref<PaymentOption[] | null>();
+
+  async function getPaymentMethod ( currency: string ) {
+    try {
+
+      isPaymentMethodDataLoading.value = true;
+      const res = await $api.paymentMethods.getPaymentMethods( currency )
+
+      if ( res.status === 200 ) {
+        paymentMethods.value = res.data;
+        isGetPaymentMethodsSuccessfull.value = true;
+        isPaymentMethodDataLoading.value = false;
+      } else {
+        isPaymentMethodDataLoading.value = false;
       }
+    } catch ( error: any ) {
+      console.log( error )
 
-      return{
-        isPaymentMethodDataLoading,
-        isGetPaymentMethodsSuccessfull,
-        getPaymentMethod,
-        paymentMethods
-      }
+      isPaymentMethodDataLoading.value = false;
+    }
+  }
+
+  return {
+    isPaymentMethodDataLoading,
+    isGetPaymentMethodsSuccessfull,
+    getPaymentMethod,
+    paymentMethods
+  }
 }
