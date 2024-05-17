2713dfd3c59e3b0fe559cf3baea2c035d299406b
“charles-brij” <charles@brij.africa>
2024-05-17 12:41:43 +0000

    fix invoie

--- components/OPTInput.vue
+++ components/OPTInput.vue
@@ -1,69 +1,68 @@
 <template>
-    <div class="flex flex-col gap-y-8  items-center">
-        <!-- text -->
-        <div class="flex flex-col">
-            <p class="text-2xl font-bold text-teal-900 text-center">Confirm OTP</p>
-            <p class="text-sm">A 6-digit Code has been sent to your number</p>
-        </div>
-        <!-- Pin code fields -->
-        <div class="pin-code flex flex-row gap-x-1">
-            <input class="pin-code" v-for="(digit, index) in digitsPin" :key="index" :ref="`digitInput${index}`"
-                v-model="digitsPin[index]" type="number" maxlength="1" @input="handleInput(index)"
-                @keydown="handleKeydown(index, $event)" />
-        </div>
+  <div class="flex flex-col gap-y-8 items-center">
+    <!-- text -->
+    <div class="flex flex-col">
+      <p class="text-2xl font-bold text-teal-900 text-center">Confirm OTP</p>
+      <p class="text-sm">A 6-digit Code has been sent to your number</p>
     </div>
+    <!-- Pin code fields -->
+    <div class="pin-code flex flex-row gap-x-1">
+      <input
+        class="pin-code"
+        v-for="(digit, index) in digitsPin"
+        :key="index"
+        :ref="`digitInput${index}`"
+        v-model="digitsPin[index]"
+        type="number"
+        maxlength="1"
+        @input="handleInput(index)"
+        @keydown="handleKeydown(index, $event)" />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-import { useCampaignStore } from '~/store/campaign'
+import { useCampaignStore } from '~/store/campaign';
 
-// intsance of payment store
 const campaignStore = useCampaignStore();
-
 
 //props
 const props = defineProps<{
-    digitsPin: string[]
-}>()
+  digitsPin: string[];
+}>();
 
 // emit
-const emit = defineEmits(['on-complete:digitsPin'])
+const emit = defineEmits(['on-complete:digitsPin']);
 
 // digits
 // const digitsPin = reactive(['', '', '', '', '', ''])
-const digitsPin = ref<string[]>(props.digitsPin)
+const digitsPin = ref<string[]>(props.digitsPin);
 
 function handleInput(index: number) {
-    if (digitsPin.value[index] !== '') {
-        if (index < digitsPin.value.length - 1) {
-            const nextInput: any = document.querySelector(
-                `.pin-code input:nth-child(${index + 2})`
-            )
-            if (nextInput) {
-                nextInput.focus()
-            }
-        } else {
-            // All inputs are filled, trigger your action here
-            emit('on-complete:digitsPin', digitsPin.value.join(''))
-        }
+  if (digitsPin.value[index] !== '') {
+    if (index < digitsPin.value.length - 1) {
+      const nextInput: any = document.querySelector(
+        `.pin-code input:nth-child(${index + 2})`
+      );
+      if (nextInput) {
+        nextInput.focus();
+      }
+    } else {
+      // All inputs are filled, trigger your action here
+      emit('on-complete:digitsPin', digitsPin.value.join(''));
     }
+  }
 }
 
 function handleKeydown(index: number, event: any) {
-    if (
-        event.key === 'Backspace' &&
-        digitsPin.value[index] === '' &&
-        index > 0
-    ) {
-        const previousInput: any = document.querySelector(
-            `.pin-code input:nth-child(${index})`
-        )
-        if (previousInput) {
-            previousInput.focus()
-        }
+  if (event.key === 'Backspace' && digitsPin.value[index] === '' && index > 0) {
+    const previousInput: any = document.querySelector(
+      `.pin-code input:nth-child(${index})`
+    );
+    if (previousInput) {
+      previousInput.focus();
     }
+  }
 }
-
-
 
 //  async function resendOtp() {
 //   this.pageLoading = true
@@ -83,15 +82,15 @@ function handleKeydown(index: number, event: any) {
 </script>
 <style scoped>
 .pin-code input {
-    border: 1.5px solid #c8cace;
-    border-radius: 8px;
-    text-align: center;
-    width: 55px;
-    height: 60px;
-    font-size: 34px;
-    color: var(--color-primary);
+  border: 1.5px solid #c8cace;
+  border-radius: 8px;
+  text-align: center;
+  width: 55px;
+  height: 60px;
+  font-size: 34px;
+  color: var(--color-primary);
 
-    /* @media (min-device-width: 700px) and (max-device-width: 1524px) {
+  /* @media (min-device-width: 700px) and (max-device-width: 1524px) {
     width: 43px !important;
     height: 50px !important;
     font-size: 24px !important;
@@ -99,24 +98,22 @@ function handleKeydown(index: number, event: any) {
 }
 
 .pin-code input:focus {
-    border: 1px solid var(--color-primary);
-    outline: none;
+  border: 1px solid var(--color-primary);
+  outline: none;
 }
 
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
-    appearance: none;
-    margin: 0;
+  appearance: none;
+  margin: 0;
 }
 
-
-
 .secondary-custom-bg-color {
-    background-color: #F9AB10;
-    color: var(--color-primary);
+  background-color: #f9ab10;
+  color: var(--color-primary);
 }
 
 .secondary-custom-bg-color:hover {
-    background-color: #f0af2c;
+  background-color: #f0af2c;
 }
 </style>
